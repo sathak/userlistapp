b0eb230f995ccea1e0625ff9a3f53dea3f643ccd
smartdevelopers <sathak318@gmail.com>
2019-12-30 09:29:04 +0300

    supporting files

---
+++ README.md
@@ -0,0 +1,19 @@
+Download the Project.
+
+Configure your api url in src/config/config.js
+```
+apiurl: your API URL
+```
+Install all dependencies using the command
+```
+npm install
+```
+Run the command on project directory
+```
+npm start
+```
+[See live result](https://0521q.csb.app/userlist)
+
+Deployed in Heroku and find the link below
+
+[Deployed in heroku cloud](https://user-webapp.herokuapp.com)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    entry:  ["@babel/polyfill", "./src/index.js"],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                exclude: /(node_modules|bower_components)/,
+        test: /\.(js|jsx)$/, //Check for all js files
+        loader: 'babel-loader',
+        query: {
+            presets: ['react', 'es2015', 'stage-3']
+        }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    
+  resolve: {
+    extensions: [ '.js', '.jsx', '.css'],
+    modules: [
+      "node_modules"
+    ] 
+  },
+   externals: {
+    // require("jquery") is external and available
+    //  on the global var jQuery
+    "jquery": "jQuery"
+  },
+    devServer: {
+        contentBase: './public',
+        historyApiFallback: true,
+    }
+};
